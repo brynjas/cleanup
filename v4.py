0331d1fae8b2b43849342d52149541d1f8786350
brynjas <brynja13@ru.is>
2016-12-07 16:43:19 +0000

    vinna að öllum skrám og paths

--- v4.py
+++ v4.py
@@ -1,33 +1,56 @@
 import os,os.path,shutil,fnmatch, pathlib
 from pprint import pprint
 import re
+import glob
 
 
 root = os.getcwd()
 directory_files = os.listdir(root)
 download = 'download'
 
+#AllPaths = glob.glob("*/*/*")
+#pprint(AllPaths)
+
+#for file in globPaths
+#def get_subfolders():
+#l = []
+#for path in AllPaths:
+
+#	l.append(path.split('/')[1:])
+#pprint(l)
+
 
 
 
 #find_seasons= re.findall(r'S(\d+)|Season\s')
 
+def get_paths():
+	#get all folders and files in downloads 
+	paths = []
+	for path, subdirs, files in os.walk(download):
+		#print( subdirs)
+		for name in files:
+			#pprint (os.path.join(path, name))
+			paths.append(os.path.join(path, name))
+	
+	return paths #All_folder_files
 
+pprint(get_paths())
 
-#print('root directory ',root)
-#print('directory files ', directory_files)
-#print(os.listdir("download")) 
 
 def get_files():
 	#get all folders and files in downloads 
-	All_folder_files = []
+	All_files = []
+	all_folders = []
 	for path, subdirs, files in os.walk(download):
+		#print( subdirs)
 		for name in files:
-			All_folder_files.append(name)
+			All_files.append(name)
        		#pprint (os.path.join(path, name))
-	return All_folder_files
+	return All_files #All_folder_files
+	#return all_folders
 
-pprint(get_files())
+#pprint(get_files())
 
 def get_tv_shows():
 	All_folder_files = get_files()
@@ -36,16 +59,8 @@ def get_tv_shows():
 	#pprint(files)
 	return files
 
-pprint(get_tv_shows())
-
-
-
-
-
-
-
-
-
+#pprint(get_tv_shows())
+#pprint(get_files())
 
 
 

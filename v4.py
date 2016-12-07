e83c444640fb2cd080be6ee07873825599635549
brynjas <brynja13@ru.is>
2016-12-07 18:07:33 +0000

    komin með þegar ég bý til möppur

--- v4.py
+++ v4.py
@@ -32,14 +32,12 @@ def get_paths():
 		for name in files:
 			#pprint (os.path.join(path, name))
 			paths.append(os.path.join(path, name))
-	
 	return paths #All_folder_files
 
-pprint(get_paths())
+#pprint(get_paths())
 
 
 def get_files():
-	#get all folders and files in downloads 
 	All_files = []
 	all_folders = []
 	for path, subdirs, files in os.walk(download):
@@ -59,6 +57,57 @@ def get_tv_shows():
 	#pprint(files)
 	return files
 
+
+folders = []
+files = []
+def subfolders():
+	paths = get_paths()
+	l = []
+	for i in paths:
+		if '.DS_Store' in str(i):
+				continue
+		a = (i.split('/')[1:])
+		s =  re.split(r'([\d])', a[0])[0]
+		if len(a) > 1:
+			folders.append(a)
+			#print('a[0]: ' ,a[0])
+			
+			if s not in l:
+				s =  re.split(r'([\d])', a[0])[0]
+				#print('s: ',str(s))
+				l.append(s)
+		else:
+			files.append(a)
+			#print('a er: ',a)
+			if s not in l:
+				l.append(s)
+	return l
+	
+def mkdir():
+	l = subfolders()
+
+	for folder in l:
+		s = 'download/'+ folder
+		#print(s)
+		try:
+			os.makedirs(s)
+		except:
+			pass
+
+	#pprint(l)
+	
+
+
+
+
+
+
+
+
+subfolders()
+
+
+#pprint(subfolders())
 #pprint(get_tv_shows())
 #pprint(get_files())
 

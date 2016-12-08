a6afe9949017f072f1d43428a47b799e393d3121
brynjas <brynja13@ru.is>
2016-12-08 15:55:43 +0000

    að flokka

--- v4.py
+++ v4.py
@@ -7,7 +7,7 @@ from guessit import *
 
 root = os.getcwd()
 directory_files = os.listdir(root)
-download = 'download'
+download = 'downloads'
 
 
 VideoTypes = ('.wmv', '.mov', '.avi', '.divx', '.mpeg', '.mpg', '.m4p', '.3gp', '.amv', '.qt', '.rm', '.swf', '.mp4', '.mkv')
@@ -24,6 +24,39 @@ SubTypes = ('.jss', '.smx', '.sup', '.srt', '.ssa', '.fab', '.sst', '.tfa', '.us
 		        #     ('mimetype', 'video/x-msvideo'),
 		        #     ('type', 'episode')])
 
+def make_Movie_folders(movie):
+	Bio = os.getcwd() + '/downloads/Movies'
+	try:
+		os.makedirs(Bio)
+	except:
+		pass
+	# make folders for all the movies and shows 
+	try:
+		os.makedirs(Bio+'/'+movie)
+	except:
+		pass
+	MovieFolder = Bio+'/'+movie + '/'
+	
+	return MovieFolder
+
+def make_Tv_folders(show):
+	
+	Tv = os.getcwd() + '/downloads/TVShows'
+	try:
+		os.makedirs(Tv)
+	except:
+		pass
+	# make folders for all the movies and shows 
+	try:
+		os.makedirs(Tv+'/'+show)
+	except:
+		pass
+	
+	TvFolder = Tv+'/'+show + '/'
+	return TvFolder
+	
+
+
 def get_tv_shows():
 	paths = []
 	file = []
@@ -34,14 +67,18 @@ def get_tv_shows():
 			pp = os.path.join(path, name)
 			if name.endswith(VideoTypes):
 				g = guessit(name)
-				filename = g['title'].upper()
+				filename = g['title'].title()
 				if g['type'] == 'episode':
 					#episodes.append(pp)
+					dst = make_Tv_folders(filename)
 					try:
 						episode.setdefault(filename, set()).add(pp)
 						#episode[filename] = pp
+						shutil.move(pp, dst)
 					except:
 						print('ex ' ,filename)
+
+	
 	return episode
 
 def get_movies():
@@ -52,30 +89,109 @@ def get_movies():
 
 	for path, subdirs, files in os.walk(download):
 		for name in files:
-			pp = os.path.join(path, name)
+			pp = os.path.join(path, name) # the src path 
 			#file.append(name)
 			#pprint(name)	
 			if name.endswith(VideoTypes):
 				
 				g = guessit(name)
-				filename = g['title'].upper()
+				filename = g['title'].title()
 				
 				if g['type'] == 'movie':
 					#movies.append(name)
+					dst = make_Movie_folders(filename)
+					print(pp, dst)
+
 					try:
 						movie.setdefault(filename, set()).add(pp)
 						#movie[filename] == pp
+						shutil.move(pp, dst)
+
 					except:
 						print('ex ',filename)
 
+
+
+
+
 	return movie
 
+get_movies()
+get_tv_shows()
 
-pprint('movies')
-pprint(get_movies())
-pprint('tv shows')
-pprint(get_tv_shows())
 
+
+
+
+
+
+
+
+def removeInFolders():
+	#make_New_Folders()
+	
+	movies = get_movies()
+	tvShows = get_tv_shows()
+
+	for i in movies.keys():
+		l = list(movies[i])
+		#print(l)
+		for path in l:
+			movie = path.split('/')
+			if len(movie) == 2:
+				src = path
+				dst =  os.getcwd() + '/downloads/Movies/'+ i + '/'
+				#print(src,dst)
+				shutil.move(src,dst)
+			if len(movie) > 2:
+				folder = movie[1]
+				folderCheck = folder.split(' ')[0]
+				fileCheck = file.split('.')
+				if fileCheck == folderCheck:
+					shutil(src , dst)
+				
+
+				print(folder , file)
+
+		#['downloads/Despicable Me 2/Despicable.Me.2.2013.BRRip.Isl.Tal.avi']
+		#['downloads/12 Years a Slave (2013) 1080p [ODI].mp4']
+
+	
+
+
+
+
+	'''for i in movies.keys():
+		for j in movies.values():
+			path = ''.join(j).split('/')
+			src = ''.join(j)
+			print(j)
+			dst =  os.getcwd() + '/downloads/Movies/'+ i + '/'
+			#isfolder =''.join(path[1:])
+
+			print('shutil : ', src, dst)
+			print('title: ' , i)
+			#try:
+			#	shutil.move(src, dst)
+			#except:
+			#	print('Gat ekki Fært: ' , src)
+
+				#shutil.move(pathsString,)'''
+			
+				
+							
+			#print(i,j)
+			#print(path)
+	#os.path.isdir("bob")
+	#os.listdir("."):
+
+
+			
+			#try:
+				#pass
+			#	shutil.move(r,p)
+			#except:
+			#	print('Tokst ekki að flytja skrár')	
 
 
 
@@ -115,54 +231,6 @@ def subfolders():
 	return l
 
 	
-def mkdir():
-	l = subfolders()
-	#print(l)
-	for folder in l:
-		s = 'downloads/'+ folder
-		print(s)
-		try:
-			os.makedirs(s)
-		except:
-			print('Gat ekki búið til möppuna ', s)
-
-	#pprint(l)
-	
-
-def removeInFolders():
-	folders = []
-	files = []
-	paths = get_paths()
-	l = subfolders()
-	for i in paths:
-		if '.DS_Store' in str(i):
-				continue
-		a = (i.split('/')[1:])
-		s =  re.split(r'([\d])| -', a[0])[0]
-		
-		if len(a) > 1:
-			p = root +'/downloads/'+s+'/'
-			r = root + '/downloads/'+a[0]
-			print('p2 er : ', p)
-			print('r2 er : ', r)
-
-			
-			try:
-				shutil.move(r, p )
-			except:
-				print('Tokst ekki að flytja skrár')			
-			
-		else:
-			p = root +'/downloads/'+s+'/'
-			r = root + '/downloads/'+a[0]
-			print('p er : ', p)
-			print('r er : ', r)
-			
-			try:
-				#pass
-				shutil.move(r,p)
-			except:
-				print('Tokst ekki að flytja skrár')	
 
 
 
